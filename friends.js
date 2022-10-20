2483182da4b8aeb395474311cd7160a7eabcb8d4
Katherine Blaine <blaine.katherine12@gmail.com>
2022-10-20 16:58:34 -0600

    Add good friend

--- friends.js
+++ friends.js
@@ -1,7 +1,10 @@
-var friend = "Bob"
+var friend = "Max"
 
 if (friend === "Bob") {
     console.log("You are my best friend!");
+} else if (friend === "Max") {
+    console.log("You are my good friend!")
 } else {
-    console.log("You are NOT my best friend!")
+    console.log("You are not my friend!")
 }
+
